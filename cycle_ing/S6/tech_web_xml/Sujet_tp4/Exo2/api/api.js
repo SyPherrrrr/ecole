cf5d879ba95e7fe3501c7af66b89675e759621e3
SyPherrrrr <boussaoui.elias+1@gmail.com>
2024-05-03 21:49:57 +0200

    tp4 inshallah dernier commit

--- cycle_ing/S6/tech_web_xml/Sujet_tp4/Exo2/api/api.js
+++ cycle_ing/S6/tech_web_xml/Sujet_tp4/Exo2/api/api.js
@@ -3,24 +3,39 @@
 // import du module Express
 let express = require('express');
 let app = express();
+const db = require('./api/data/db.json');
 
+app.get('/genres/', (req, res) => {
 
-// export de notre application vers le serveur principal
-module.exports = app;
+    const genres = JSON.stringify(db.genres);
 
-const db = require('./data/db.json');
+    res.set('Content-Type', 'application/json');
 
-
-// Liste des genres
-app.get('/genres/', function(req, res) {
-    console.log("jsuis laaaaaaaa");
-
-    const genres = JSON.stringify(db.json);
-
-    res.set('Content-Type','application/json');
-    
     res.send(genres);
 
     res.end();
 
 });
+
+app.get('/genres/:genreId/artists', (req, res) => {
+
+    const artists = db.artists.filter((elem) => elem.genreId === req.params.genreId);
+
+    res.set('Content-Type', 'application/json');
+
+    res.send(artists);
+});
+
+app.get('/artists/:artistId/albums', (req, res) => {
+
+    const albums = db.albums.filter((elem) => elem.artistId === req.params.artistId);
+
+    res.set('Content-Type', 'application/json');
+
+    res.send(albums);
+
+});
+
+
+// export de notre application vers le serveur principal
+module.exports = app;
--- cycle_ing/S6/tech_web_xml/Sujet_tp4/Exo2/serveur.js
+++ cycle_ing/S6/tech_web_xml/Sujet_tp4/Exo2/serveur.js
@@ -3,6 +3,8 @@
 // Importation express
 let express = require('express');
 let app = express();
+let api = require('./api/api');
+
 
 // Indiquer à Express de servir le contenu statique depuis le répertoire 'public'
 // Route par default
@@ -15,7 +17,6 @@ app.listen(8080, () => {
 });
 
 
-let api = require('./api/api');
 
 // Si je fais localhost:8080/api on fait ce qu'il y a dans le fichier ./api/api
 // Comme un if (le chemin est /api ) alors on fait ce qu'il y a dans api (possible de mettre function (){...} et l'éxecuter)
